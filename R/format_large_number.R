750d9ee4f83ffcba76ef6beb2d5acb4409f7709d
jteske54 <teske.jacob@gmail.com>
2023-02-12 17:42:27 -0600

    changed default hello.R to format_large_number

--- R/format_large_number.R
+++ R/format_large_number.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,9 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+
+format_large_number <- function(x, decimals = 2, short = FALSE){
+
 }
+
+fln <- format_large_number
